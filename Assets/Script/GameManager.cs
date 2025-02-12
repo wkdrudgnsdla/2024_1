d3abeb1786fc728c742f83463d2a8598914b9d42
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-02-12 11:41:07 +0900

    치트 F3번 완성

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     PlayerCam cam;
+    Camera PCam;
     PlayerMove player;
     MultiTerrainChecker MTC;
 
-    public float sec;
+    public float sec { get;set;}
+    public float StartCountdown;
 
     public int SCount;
     public int MCount;
-    public float StartCountdown;
 
     public bool startRace;
     public bool OutTrack;
@@ -21,6 +23,7 @@ public class GameManager : MonoBehaviour
     public void Awake()
     {
         cam = GameObject.Find("PlayerCam").GetComponent<PlayerCam>();
+        PCam = cam.GetComponent<Camera>();
         player = GameObject.Find("Player").GetComponent<PlayerMove>();
         MTC = player.GetComponent<MultiTerrainChecker>();
     }
@@ -40,6 +43,17 @@ public class GameManager : MonoBehaviour
             startRace = true;
         }
 
+        if(OutTrack)
+        {
+            player.moveSpeed = 5;
+            PCam.fieldOfView = Mathf.Lerp(PCam.fieldOfView, 40f, Time.deltaTime);
+        }
+        else
+        {
+            player.moveSpeed = 10;
+            PCam.fieldOfView = Mathf.Lerp(PCam.fieldOfView, 60f, Time.deltaTime);
+        }
+
         if(MTC.layerIndex == 1 || MTC.layerIndex == 3)
         {
             OutTrack = false;
@@ -51,6 +65,7 @@ public class GameManager : MonoBehaviour
 
         if(startRace)
         {
+            player.rb.interpolation = RigidbodyInterpolation.Interpolate;
             player.moveable = true;
             sec += Time.deltaTime;
 
@@ -73,5 +88,35 @@ public class GameManager : MonoBehaviour
             SCount = 0;
             MCount += 1;
         }
+    }
+
+    public void StartGame()
+    {
+        player.rb.velocity = Vector3.zero;
+        player.rb.angularVelocity = Vector3.zero;
+        player.rb.interpolation = RigidbodyInterpolation.None;
+
+        sec = 0;
+        MCount = 0;
+        SCount = 0;
+
+        startRace = false;
+        StartCountdown = 4;
+        OutTrack = false;
+
+        player.moveSpeed = 10f;
+        player.turnSpeed = 1f;
+        player.brakeForce = 10f;
+        player.turnDamping = 1f;
+        player.extraGravity = 5f;
+        player.currentSpeed = 0;
+        player.moveable = false;
+        player.transform.position = new Vector3(74.4f, 2.5f, 5.5f);
+        player.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+
+        cam.transform.position = new Vector3(843, 1029, 275);
+        cam.followSpeed = 1;
+        cam.rotationSpeed = 2;
     }
 }
