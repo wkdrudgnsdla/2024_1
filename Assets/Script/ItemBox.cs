0a533553899f357015f617b69b3a7c6e3982332d
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-02-17 17:04:09 +0900

    아이템 UI 완성

--- Assets/Script/ItemBox.cs
+++ Assets/Script/ItemBox.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class ItemBox : MonoBehaviour
 {
+    public ItemUIManager IUM;
     public PlayerMove player;
     public GameManager GM;
     public Camera PCam;
 
     private int _ItemNum;
 
-    private int ItemNum
+    public int ItemNum
     {
         get
         {
@@ -27,6 +29,7 @@ public class ItemBox : MonoBehaviour
 
     private void Awake()
     {
+        IUM = GameObject.Find("GameManager").GetComponent<ItemUIManager>();
         player = GameObject.Find("Player").GetComponent<PlayerMove>();
         GM = GameObject.Find("GameManager").GetComponent<GameManager>();
         PCam = GameObject.Find("PlayerCam").GetComponent<Camera>();
@@ -38,12 +41,14 @@ public class ItemBox : MonoBehaviour
         changeable = true;
     }
 
+
     private void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             if(changeable)
             {
+                IUM.UseItme = true;
                 ItemNum = Random.Range(1, 6 + 1);
                 ItemControl(_ItemNum);
             }
@@ -80,12 +85,15 @@ public class ItemBox : MonoBehaviour
 
     private void Item1()
     {
+        IUM.itemnum = 1;
         GM.cash += 1000000;
         Debug.Log("1000000만원");
     }
 
     private void Item2()
     {
+        IUM.itemnum = 2;
+
         GM.cash += 5000000;
         Debug.Log("5000000만원");
 
@@ -93,19 +101,23 @@ public class ItemBox : MonoBehaviour
 
     private void Item3()
     {
+        IUM.itemnum = 3;
+
         GM.cash += 10000000;
         Debug.Log("10000000만원");
     }
 
     private void Item4()
     {
+        IUM.itemnum = 4;
         player.rb.AddForce(player.transform.right * 1000 * Time.deltaTime, ForceMode.Impulse);
-        PCam.fieldOfView = 70f;
+        PCam.fieldOfView = 75f;
         Debug.Log("속도 소폭 증가");
     }
 
     private void Item5()
     {
+        IUM.itemnum = 5;
         player.rb.AddForce(player.transform.right * 2000* Time.deltaTime, ForceMode.Impulse);
         PCam.fieldOfView = 80f;
         Debug.Log("속도 대폭 증가");
@@ -113,6 +125,7 @@ public class ItemBox : MonoBehaviour
 
     private void Item6()
     {
+        IUM.itemnum = 6;
         //상점 UI setActive true로 변경
     }
 }
---
+++ Assets/Script/Managers/ItemUIManager.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemUIManager : MonoBehaviour
+{
+    public GameObject ItemLogBG;
+    public GameObject Item1;
+    public GameObject Item2;
+    public GameObject Item3;
+    public GameObject Item4;
+    public GameObject Item5;
+    public GameObject Item6;
+
+    public Text ItemName;
+
+    public bool UseItme;
+
+    public int itemnum;
+
+    private void Awake()
+    {
+        ItemLogBG = GameObject.Find("ItemLogBG");
+        Item1 = GameObject.Find("Item1");
+        Item2 = GameObject.Find("Item2");
+        //Item3 = GameObject.Find("Itme3");
+        Item4 = GameObject.Find("Item4");
+        Item5 = GameObject.Find("Item5");
+        Item6 = GameObject.Find("Item6");
+
+        ItemName = GameObject.Find("ItemName").GetComponent<Text>();
+    }
+
+    private void Start()
+    {
+        ItemLogBG.SetActive(false);
+        Item1.SetActive(false);
+        Item2.SetActive(false);
+        Item3.SetActive(false);
+        Item4.SetActive(false);
+        Item5.SetActive(false);
+        Item6.SetActive(false);
+        ItemName.text = " ";
+
+        UseItme = false;
+        itemnum = 0;
+    }
+
+    void Update()
+    {
+        if(UseItme)
+        {
+            StartCoroutine(OnUIItem(itemnum));
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    IEnumerator OnUIItem(int _itemnum)
+    {
+        ItemLogBG.SetActive(true);
+        switch (_itemnum)
+        {
+            case 1:
+                Item1.SetActive(true);
+                ItemName.text = "1,000,000¸¸¿ø È¹µæ";
+                break;
+            case 2:
+                Item2.SetActive(true);
+                ItemName.text = "5,000,000¸¸¿ø È¹µæ";
+                break;
+            case 3:
+                Item3.SetActive(true);
+                ItemName.text = "10,000,000¸¸¿ø È¹µæ";
+                break;
+            case 4:
+                Item4.SetActive(true);
+                ItemName.text = "³ë¸Ö ºÎ½ºÆ® »ç¿ë";
+                break;
+            case 5:
+                Item5.SetActive(true);
+                ItemName.text = "½´ÆÛ ºÎ½ºÆ® »ç¿ë";
+                break;
+            case 6:
+                Item6.SetActive(true);
+                ItemName.text = "»óÁ¡";
+                break;
+            default:
+                break;
+        }
+        yield return new WaitForSeconds(0.8f);
+
+        Item1.SetActive(false);
+        Item2.SetActive(false);
+        Item3.SetActive(false);
+        Item4.SetActive (false);
+        Item5.SetActive(false);
+        Item6.SetActive(false);
+        ItemLogBG.SetActive(false);
+        ItemName.text = " ";
+        UseItme = false;
+    }
+}
